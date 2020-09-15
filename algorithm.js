84b855fd3e85f699fbba522606b9ad3915333353
seanhailecodes <seanhaile.codes@gmail.com>
2020-09-14 22:05:23 -0600

    Rebuilt function for Eculid's algorithm

--- algorithm.js
+++ algorithm.js
@@ -1,10 +1,11 @@
 // euclids algorithm finds the greatest common denomenator (GCD) of two integers, until the remainder is 0
 // for example gcd of 20 and 8 is 4
 
-function gcd_rec(a, b) {
-    if (b) {
-        return gcd_rec(b, a % b);
-    } else {
-        return Math.abs(a);
-    }
-}
+var gcd = function(a, b) {  
+    if (!b) {  
+        return a;  
+    }  
+ 
+    return gcd(b, a % b);  
+ };  
+ console.log(gcd(20, 8));
