3c265d09ceb50c4789010882042a8b3eab51b776
seanhailecodes <seanhaile.codes@gmail.com>
2020-09-29 23:11:22 -0600

    recursive and euclid

--- algorithm.js
+++ algorithm.js
@@ -1,11 +1,24 @@
+// recusrsion
+
+function efun(num){
+   if (num==0)
+    return 1 
+       else
+    return num * efun(num-2)
+  }
+
+console.log(efun(8))
+
+
+
 // euclids algorithm finds the greatest common denomenator (GCD) of two integers, until the remainder is 0
 // for example gcd of 20 and 8 is 4
 
 var gcd = function(a, b) {  
-    if (!b) {  
-        return a;  
-    }  
- 
-    return gcd(b, a % b);  
- };  
- console.log(gcd(20, 8));
+   if (!b) {  
+       return a;  
+   }  
+
+   return gcd(b, a % b);  
+};  
+console.log(gcd(20, 8));
