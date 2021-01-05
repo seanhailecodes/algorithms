d31635e057ebad49682ac7ff5244e5f984951ff7
Sean Haile <seanhaile.codes@gmail.com>
2021-01-04 20:41:45 -0700

    updated hackerrank function

--- algorithm.js
+++ algorithm.js
@@ -359,11 +359,28 @@ function gridTraveler2D (m, n) {
  
 function canSum
 
+//###hackerrank, Int and Str work
 
-//convert a String to Int
-const firstInteger = 4;
-const secondInteger = '12'
-
-let newSecondInt = parseInt(secondInteger, 10)
-let sum = (firstInteger + newSecondInt)
-console.log(sum)
+function performOperation(secondInteger, secondDecimal, secondString) {
+   // Declare a variable named 'firstInteger' and initialize with integer value 4.
+       const firstInteger = 4;
+   
+   // Declare a variable named 'firstDecimal' and initialize with floating-point value 4.0.
+       const firstDecimal = 4.0;
+       
+   // Declare a variable named 'firstString' and initialize with the string "HackerRank".
+       const firstString = 'HackerRank ';
+   
+   // Write code that uses console.log to print the sum of the 'firstInteger' and 'secondInteger' (converted to a Number        type) on a new line.
+   
+       let newSecondInt = parseInt(secondInteger, 10)
+       let sum = (firstInteger + newSecondInt)
+       console.log(sum)
+   // Write code that uses console.log to print the sum of 'firstDecimal' and 'secondDecimal' (converted to a Number            type) on a new line.
+   
+       console.log( parseFloat(secondDecimal) + 
+       parseFloat(firstDecimal) )
+  
+   // Write code that uses console.log to print the concatenation of 'firstString' and 'secondString' on a new line. The        variable 'firstString' must be printed first.
+       console.log(firstString + secondString)
+}
