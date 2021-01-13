b361415ad8e14a6b806d1cb0495eb556a21e47a0
seanhailecodes <seanhaile.codes@gmail.com>
2021-01-12 21:29:06 -0700

    tip costs

--- hackerrank.js
+++ hackerrank.js
@@ -26,7 +26,7 @@ let sumI = i + i2
 
 
 //### Print the sum of the double variables on a new line.
-
+``
 let sumD = parseFloat(d + d2).toFixed(1)
  console.log(sumD)        
 
@@ -38,4 +38,16 @@ let concat = s + s2
 
 //### The 's' variable above should be printed first.
 
+}
+
+
+//Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip), 
+//and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost. Round the result to the nearest integer.
+
+// TIP%*MEAL/100 is how to get the percent of monies owed
+
+let total_cost=meal_cost+(tip_percent*meal_cost/100)+(tax_percent*meal_cost/100)
+// console.log(total_cost) = 15.36
+let rounded_cost=Math.round(total_cost)
+console.log(rounded_cost)
 }
